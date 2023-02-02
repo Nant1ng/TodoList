3d8d41c080182f213162a2ac3c0ac0616720d396
Andrés <andressantana99@hotmail.se>
2023-02-02 23:28:18 +0100

    Get todos works

--- components/Todolist.js
+++ components/Todolist.js
@@ -1,6 +1,6 @@
 import { db } from "@/firebase";
-import { addDoc, collection, getDocs } from "firebase/firestore";
-import { useState } from "react";
+import { addDoc, collection, getDocs, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 import styled from "styled-components";
 
@@ -8,8 +8,13 @@ const Title = styled.h1`
   color: #6b0a7f;
 `;
 
+const TodoTitle = styled.h3`
+  color: #6b0a7f;
+`;
+
 function Todolist() {
   const [todoTitle, setTodoTitle] = useState("");
+  const [todos, setTodos] = useState([]);
 
   const submitHandler = async () => {
     await addDoc(collection(db, "todo"), {
@@ -17,6 +22,18 @@ function Todolist() {
     });
     setTodoTitle("");
   };
+
+  useEffect(() => {
+    async function getTodos() {
+      const todos = (await getDocs(collection(db, "todo"))).docs.map((doc) => {
+        const data = doc.data();
+        data.id = doc.id;
+        return data;
+      });
+      setTodos(todos);
+    }
+    getTodos();
+  }, [db]);
 
   return (
     <div>
@@ -28,6 +45,15 @@ function Todolist() {
           onChange={(e) => setTodoTitle(e.target.value)}
         />
         <button onClick={submitHandler}>Submit</button>
+      </div>
+      <div>
+        {todos.map((todo) => {
+          return (
+            <div key={todo.id}>
+              <TodoTitle>{todo.title}</TodoTitle>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
